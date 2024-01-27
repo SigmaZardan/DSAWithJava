f1acfcfc850122c2a28f053399417d905996f4c7
Bibek Bhujel <biekbhujel077@gmail.com>
2024-01-27 17:55:20 +0545

    Description of brute force added

--- src/array/easy/RotateArrayByDPlacesToLeft.java
+++ src/array/easy/RotateArrayByDPlacesToLeft.java
@@ -9,16 +9,26 @@ public class RotateArrayByDPlacesToLeft {
         System.out.println(Arrays.toString(arr));
     }
 
+
+    // this is brute force approach which seems optimal in this case
+    // O(3n) -> O(n) time complexity
+    // O(k) extra space
+
     private static void rotate(int[] arr, int k) {
         int[] temp = new int[k];
         int n = arr.length;
 
+        // populate all the elements from 0th index < k into the temporary array of size k
+        // the numbers are added in reverse order in the temporary array
         for (int i = 0; i < k; i++) {
             temp[k - i - 1] = arr[i];
         }
+        // left shift all the numbers from k < n
         for (int i = k; i < n; i++) {
             arr[i - k] = arr[i];
         }
+        // finally populate the elements from the temporary array to the
+        // input array from the last
         for (int i = 0; i < k; i++) {
             arr[n - i - 1] = temp[i];
         }
