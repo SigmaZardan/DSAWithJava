25369d4c93377b966b4784042a87c67329f57097
Bibek Bhujel <biekbhujel077@gmail.com>
2024-01-28 14:32:34 +0545

    Optimal approach for solvin rotation of an array by d places

--- src/array/easy/RotateArrayByDPlacesToLeft.java
+++ src/array/easy/RotateArrayByDPlacesToLeft.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RotateArrayByDPlacesToLeft {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        rotate(arr, 3);
+        optimal(arr, 3);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -33,4 +33,31 @@ public class RotateArrayByDPlacesToLeft {
             arr[n - i - 1] = temp[i];
         }
     }
+
+
+    // optimal approach
+    // what can be the optimal approach
+    // write a reverse function
+    // reverse array from 0 , k - 1
+    // reverse array from k, n - 1
+    // reverse array from 0, n - 1
+    // time complexity -> O(n)
+    // space complexity -> O (1 ) extra space
+    private static void optimal(int[] arr, int k) {
+        int n = arr.length;
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
 }
